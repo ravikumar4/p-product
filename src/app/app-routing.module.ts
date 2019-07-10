d3c18bb66efefb6449cdafcfb55dea8e4a3735c0
Ravi Kumar <ravi.kumar4@gmail.com>
2019-07-10 00:46:47 -0400

    day 2 changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,17 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ProductListComponent } from './component/product-list/product-list.component';
 import { ProductDetailComponent } from './component/product-detail/product-detail.component';
+import { WelcomeComponent } from './component/welcome/welcome.component';
 
 
 const routes: Routes = [{ path: 'products', component: ProductListComponent },
 {
   path: 'products/:id',
   component: ProductDetailComponent
+},
+{
+  path: 'welcome',
+  component: WelcomeComponent
 }];
 
 @NgModule({
--- src/app/component/welcome/welcome.component.ts
+++ src/app/component/welcome/welcome.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./welcome.component.scss']
 })
 export class WelcomeComponent implements OnInit {
-  public pageTitle = 'test';
+  public pageTitle = 'Welcome';
    // public z: string[] = ['test'];    // for showing how to define data type.
 
   constructor() { }
